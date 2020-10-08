0635d1804773b38d46cdf526e9f97507dc8a3863
Jeremy Ephron <jeremyephron@gmail.com>
2020-10-07 20:45:18 -0700

    server: runserver.sh

--- forager_server/runserver.sh
+++ forager_server/runserver.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
+export GOOGLE_APPLICATION_CREDENTIALS="$(pwd)/gcloud-credentials.json"
 python3 manage.py makemigrations && python3 manage.py migrate
 python3 manage.py runserver
