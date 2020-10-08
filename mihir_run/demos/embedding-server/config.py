11c1a43c071b0d3e3933d3bb478a873a90e2a962
Mihir Garimella <mihir.garimella@gmail.com>
2020-10-07 23:06:09 -0400

    First pass at integrating index into embedding server

--- mihir_run/demos/embedding-server/config.py
+++ mihir_run/demos/embedding-server/config.py
@@ -1,12 +1,18 @@
-GCP_PROJECT = 'visualdb-1046'
-GCP_ZONE = 'us-central1-a'
-GCP_MACHINE_TYPE = 'n2-highcpu-2'
+GCP_PROJECT = "visualdb-1046"
+GCP_ZONE = "us-central1-a"
+GCP_MACHINE_TYPE = "n2-highcpu-2"
 
 OUTPUT_FILENAME = "lvis-embeddings.npy"
 MAPPER_CONTAINER = "gcr.io/visualdb-1046/mihir-demo-full-image-embedding"
 
-QUERY_CLEANUP_TIME = 60 * 60 # 1 hour
+QUERY_CLEANUP_TIME = 60 * 60  # 1 hour
 N_MAPPERS = 1000
 N_RETRIES = 100
 
 UPDATE_INTERVAL = 1
+
+EMBEDDING_LAYER = "res5"
+EMBEDDING_DIM = 2048
+INDEX_NUM_CENTROIDS = 1024
+INDEX_SUBINDEX_SIZE = 25000
+INDEX_NUM_QUERY_PROBES = 4
