acb35bef06aa199c84833d62cf369a667f8bc3e4
Mihir Garimella <mihir.garimella@gmail.com>
2020-11-03 15:22:04 -0800

    Removed orchestrator prints

--- mihir_run/src/knn/jobs/jobs.py
+++ mihir_run/src/knn/jobs/jobs.py
@@ -232,9 +232,6 @@ class MapReduceJob:
 
         request = self._construct_request(chunk)
 
-        print(mapper_url)
-        print(request)
-
         for i in range(self.n_retries):
             start_time = time.time()
             end_time = start_time
