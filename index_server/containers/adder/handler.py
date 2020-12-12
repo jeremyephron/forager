63cd5224086bec2b31e78c4f695831e22499720d
Mihir Garimella <mihir.garimella@gmail.com>
2020-12-12 01:00:51 -0500

    Small updates to adder container

--- index_server/containers/adder/handler.py
+++ index_server/containers/adder/handler.py
@@ -10,7 +10,7 @@ from knn.utils import JSONType
 import config
 
 
-class TrainableIndex:
+class Index:
     def __init__(self, index_dict: Dict[str, str], shard_tmpl: str):
         reduction = index_dict.get("reduction")
         index_dir = index_dict["index_dir"]
@@ -42,10 +42,10 @@ class IndexBuildingMapper(Mapper):
         self.shard_tmpl = config.SHARD_INDEX_NAME_TMPL.format(self.worker_id)
 
     async def initialize_job(self, job_args) -> InteractiveIndex:
-        job_args["indexes"] = [
-            TrainableIndex(index_dict, self.shard_tmpl)
-            for index_dict in job_args["indexes"]
-        ]
+        job_args["indexes"] = {
+            index_type: Index(index_dict, self.shard_tmpl)
+            for index_type, index_dict in job_args["indexes"].items()
+        }
         return job_args
 
     # input = path to a np.save'd Dict[int, np.ndarray] where each value is N x D
@@ -58,7 +58,10 @@ class IndexBuildingMapper(Mapper):
         ).item()  # type: Dict[int, np.ndarray]
 
         # Step 2: Add to on-disk indexes
-        return [index.add(embedding_dict) for index in job_args["indexes"]]
+        return {
+            index_type: index.add(embedding_dict)
+            for index_type, index in job_args["indexes"].items()
+        }
 
     async def postprocess_chunk(
         self,
