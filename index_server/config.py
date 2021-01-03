5d4b703d1005be6107df27c3972be02dbab8f156
Mihir Garimella <mihir.garimella@gmail.com>
2021-01-02 22:36:57 -0500

    Mess around with chunk size for Mapper and Adder

--- index_server/config.py
+++ index_server/config.py
@@ -19,14 +19,14 @@ CLUSTER_REUSE_EXISTING = True
 CLUSTER_MOUNT_DIR = Path("~/forager/mount").expanduser().resolve()
 
 MAPPER_NUM_RETRIES = 3
-MAPPER_CHUNK_SIZE = lambda nproc: 4
-MAPPER_REQUEST_MULTIPLE = lambda nproc: nproc / 2
+MAPPER_CHUNK_SIZE = lambda nproc: nproc
+MAPPER_REQUEST_MULTIPLE = lambda nproc: 2
 MAPPER_REQUEST_TIMEOUT = 5 * 60  # seconds
 MAPPER_CLOUD_RUN_URL = "https://forager-index-mapper-g6rwrca4fq-uw.a.run.app"
 
 ADDER_NUM_RETRIES = 3
-ADDER_CHUNK_SIZE = lambda nproc: nproc
-ADDER_REQUEST_MULTIPLE = lambda nproc: 2
+ADDER_CHUNK_SIZE = lambda nproc: 2
+ADDER_REQUEST_MULTIPLE = lambda nproc: nproc
 ADDER_REQUEST_TIMEOUT = 10 * 60  # seconds
 
 NUM_IMAGES_TO_MAP_BEFORE_CONFIGURING_INDEX = 100
