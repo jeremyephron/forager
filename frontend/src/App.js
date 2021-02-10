e54511cd9ed94383121680dfd41fd97fc0c970db
Mihir Garimella <mihir.garimella@gmail.com>
2021-02-10 01:18:55 -0800

    Added clustering code but it's too slow, so also added standin for now

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -22,6 +22,9 @@ import imageIds from "./data/waymo";
 import "react-bootstrap-typeahead/css/Typeahead.css";
 import "./scss/theme.scss";
 
+var performClustering = require("hierarchical-clustering");
+var computeSimilarity = require("compute-cosine-similarity");
+
 const sources = [
   {id: "dataset", label: "Dataset"},
   {id: "google", label: "Google"},
@@ -39,60 +42,92 @@ const tags = [
   {id: "garbage_bin", label: "garbage bin"},
 ]
 
-const getWaymoImageSpec = (id) => {
+const images = imageIds.slice(0, 1000).map(id => {
   return {
     name: `${id}_front.jpeg`,
     url: `https://storage.googleapis.com/foragerml/waymo/train/${id}_front.jpeg`,
+    embedding: [...new Array(256)].map(() => Math.random() * 10 - 5)  // random for now
   };
-}
+})
 
-const ImageStack = ({ id, onClick, images }) => {
+const imageSimilarity = (a, b) => computeSimilarity(a.embedding, b.embedding);
+
+const ImageStack = ({ id, onClick, images, showLabel }) => {
   return (
     <a className="stack" onClick={onClick}>
       {times(Math.min(4, images.length), (i) =>
         <img key={`stack-${i}`} className="thumb" src={images[i].url}></img>
       )}
-      <div className="label">
+      {showLabel && <div className="label">
         <b>Cluster {id + 1}</b> ({images.length} image{images.length !== 1 && "s"})
-      </div>
+      </div>}
     </a>
   );
+}
+
+// TEMPORARY until we have a real clustering algorithm in
+let initialClusters = [];
+for (let i = 0; i < 100; i++) {
+  let cluster = [];
+  for (let j = 0; j < 10; j++) {
+    cluster.push(images[i * 10 + j]);
+  }
+  initialClusters.push(cluster);
 }
 
 const App = () => {
   const [source, setSource] = useState(sources[0].id);
   const [datasetQuery, setDatasetQuery] = useState([tags[0]]);
   const [googleQuery, setGoogleQuery] = useState("");
-  const [clusteringStrength, setClusteringStrength] = useState(0);
+  const [clusteringStrength, setClusteringStrength] = useState(50);
   const [selection, setSelection] = useState({});
   const [isOpen, setIsOpen] = useState(false);
-  const [clusters, setClusters] = useState(Array(100).fill(imageIds.slice(0, 5).map(getWaymoImageSpec)));
+  const [clusters, setClusters] = useState([]);
 
-  const handleKeyDown = useCallback((e) => {
+  const handleKeyDown = useCallback(e => {
     if (isOpen) {
       const { key } = e;
       let caught = true;
-      if (key === 'ArrowDown') {
+      if (key === "ArrowDown" || (clusteringStrength == 0 && key === "ArrowRight")) {
         setSelection({cluster: Math.min(selection.cluster + 1, clusters.length - 1), image: 0});
-      } else if (key === 'ArrowUp') {
+      } else if (key === "ArrowUp" || (clusteringStrength == 0 && key === "ArrowLeft")) {
         setSelection({cluster: Math.max(selection.cluster - 1, 0), image: 0});
-      } else if (key === 'ArrowRight') {
+      } else if (key === "ArrowRight") {
         setSelection({...selection, image: Math.min(selection.image + 1, clusters[selection.cluster].length - 1)});
-      } else if (key === 'ArrowLeft') {
+      } else if (key === "ArrowLeft") {
         setSelection({...selection, image: Math.max(selection.image - 1, 0)});
       } else {
         caught = false;
       }
       if (caught) e.preventDefault();
     }
-  }, [isOpen, setSelection, selection]);
+  }, [isOpen, clusteringStrength, setSelection, selection]);
 
   useEffect(() => {
-    document.addEventListener('keydown', handleKeyDown)
+    document.addEventListener("keydown", handleKeyDown)
     return () => {
-      document.removeEventListener('keydown', handleKeyDown)
+      document.removeEventListener("keydown", handleKeyDown)
     }
   }, [handleKeyDown]);
+
+  const recluster = () => {
+    if (clusteringStrength == 0) {
+      setClusters(images.map(i => [i]));
+    } else {
+      // TODO(mihirg): Figure out a way to do this that doesn't crash the browser
+      // TODO(mihirg): Cache distance matrix for faster re-clustering
+      setClusters(initialClusters);
+      // const levels = performClustering({
+      //   input: images,
+      //   distance: imageSimilarity,
+      //   linkage: "average",
+      //   maxLinkage: clusteringStrength / 100,
+      // });
+      // const clusterIndices = levels[levels.length - 1].clusters;
+      // setClusters(clusterIndices.map(cluster => cluster.map(index => images[index])));
+    }
+  };
+  useEffect(recluster, []);
 
   const toggle = () => setIsOpen(!isOpen);
 
@@ -102,14 +137,21 @@ const App = () => {
         {selection.cluster !== undefined && selection.image !== undefined &&
           <>
             <ModalHeader toggle={toggle}>
-              Cluster {selection.cluster + 1} of {clusters.length},
-              image {selection.image + 1} of {clusters[selection.cluster].length}
+              {(clusteringStrength > 0) ?
+                <span>
+                  Cluster {selection.cluster + 1} of {clusters.length},
+                  image {selection.image + 1} of {clusters[selection.cluster].length}
+                </span>:
+                <span>
+                  Image {selection.cluster + 1} of {clusters.length}
+                </span>}
               <span className="text-muted"> ({clusters[selection.cluster][selection.image].name})</span>
             </ModalHeader>
             <ModalBody>
               <p>
-                <b>Key bindings: </b> use <kbd>&uarr;</kbd> <kbd>&darr;</kbd> to move
-                between clusters, <kbd>&larr;</kbd> <kbd>&rarr;</kbd> to move between images
+                <b>Key bindings: </b> use <kbd>&uarr;</kbd> <kbd>&darr;</kbd>
+                {(clusteringStrength > 0) ? " to move between clusters, " : " or "}
+                <kbd>&larr;</kbd> <kbd>&rarr;</kbd> to move between images
               </p>
               <img src={clusters[selection.cluster][selection.image].url} />
               <Form>
@@ -121,7 +163,7 @@ const App = () => {
                     id="image-tag-bar"
                     className="typeahead-bar"
                     placeholder="Image tags"
-                    options={tags.filter((t) => !t.hide)}
+                    options={tags.filter(t => !t.hide)}
                   />
                 </FormGroup>
               </Form>
@@ -145,7 +187,7 @@ const App = () => {
           <Container fluid>
             <Form className="d-flex flex-row align-items-center">
               <FormGroup className="mb-0">
-                <select className="custom-select mr-2" onChange={(e) => setSource(e.target.value)}>
+                <select className="custom-select mr-2" onChange={e => setSource(e.target.value)}>
                   {sources.map((s) => <option value={s.id} selected={source === s.id}>{s.label}</option>)}
                 </select>
                 <ReactSVG className="icon" src="assets/arrow-caret.svg" />
@@ -159,7 +201,7 @@ const App = () => {
                     placeholder="Query tags"
                     options={tags}
                     selected={datasetQuery}
-                    onChange={(selected) => setDatasetQuery(selected)}
+                    onChange={selected => setDatasetQuery(selected)}
                   />)
                 } else if (source === "google") {
                   return (
@@ -167,7 +209,7 @@ const App = () => {
                       type="text"
                       placeholder="Query"
                       value={googleQuery}
-                      onChange={(e) => setGoogleQuery(e.target.value)}
+                      onChange={e => setGoogleQuery(e.target.value)}
                     />
                   )
                 }
@@ -184,7 +226,9 @@ const App = () => {
               <div>
                 <span className="text-nowrap">Clustering strength:</span>
                 <input className="custom-range" type="range" min="0" max="100"
-                  value={clusteringStrength} onChange={(e) => setClusteringStrength(e.target.value)} />
+                  value={clusteringStrength}
+                  onChange={e => setClusteringStrength(e.target.value)}
+                  onMouseUp={recluster} />
               </div>
             </Form>
           </Container>
@@ -200,6 +244,7 @@ const App = () => {
                     setIsOpen(true);
                   }}
                   images={images}
+                  showLabel={clusteringStrength > 0}
                 />
               )}
             </Col>
