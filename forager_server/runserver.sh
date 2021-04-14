219c4b192e2930ddc5dd3884b4e58671cbbaf1b4
Fait Poms <faitpoms@gmail.com>
2021-04-13 22:06:53 -0700

    Settings to run on server

--- forager_server/runserver.sh
+++ forager_server/runserver.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-export GOOGLE_APPLICATION_CREDENTIALS="$(pwd)/gcloud-credentials.json"
+
+#export GOOGLE_APPLICATION_CREDENTIALS="$(pwd)/gcloud-credentials.json"
 source ./cseInfo
 python3 manage.py makemigrations && python3 manage.py migrate
-python3 manage.py runsslserver
+#python3 manage.py runsslserver
+uvicorn --host 0.0.0.0 --port 8000 forager_server.asgi:application
