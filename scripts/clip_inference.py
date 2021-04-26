b3b6cfdb7932355ce099c2ac339372d46af7880e
Mihir Garimella <mihir.garimella@gmail.com>
2021-04-25 20:16:37 -0700

    parallelism in clip inference

--- scripts/clip_inference.py
+++ scripts/clip_inference.py
@@ -1,5 +1,6 @@
-from pathlib import Path
+import concurrent.futures
 import json
+from pathlib import Path
 
 import numpy as np
 import torch
@@ -7,7 +8,7 @@ import clip
 from PIL import Image
 from tqdm import tqdm
 
-BATCH_SIZE = 128
+BATCH_SIZE = 256
 IMAGE_DIR = Path("/home/mihir/waymo/train")
 EMBEDDING_DIM = 512
 
@@ -16,6 +17,7 @@ EMBEDDINGS_OUTPUT_FILENAME = "embeddings.npy"
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 model, preprocess = clip.load("ViT-B/32", device=device)
+load_image = lambda path: preprocess(Image.open(path)).unsqueeze(0).to(device)
 
 image_paths = list(IMAGE_DIR.glob("*.jpeg"))
 json.dump([str(p) for p in image_paths], Path(IMAGE_LIST_OUTPUT_FILENAME).open("w"))
@@ -27,15 +29,15 @@ embeddings = np.memmap(
     shape=(len(image_paths), EMBEDDING_DIM),
 )
 
-for i in tqdm(range(0, len(image_paths), BATCH_SIZE)):
-    # Load batch of images
-    batch_paths = image_paths[i : i + BATCH_SIZE]
-    images = [preprocess(Image.open(p)).unsqueeze(0).to(device) for p in batch_paths]
-    images = torch.cat(images)
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    for i in tqdm(range(0, len(image_paths), BATCH_SIZE)):
+        # Load batch of images
+        batch_paths = image_paths[i : i + BATCH_SIZE]
+        images = torch.cat(list(executor.map(load_image, batch_paths)))
 
-    with torch.no_grad():
-        image_features = model.encode_image(images)
-        image_features /= image_features.norm(dim=-1, keepdim=True)
-        embeddings[i : i + BATCH_SIZE] = image_features.cpu().numpy()
+        with torch.no_grad():
+            image_features = model.encode_image(images)
+            image_features /= image_features.norm(dim=-1, keepdim=True)
+            embeddings[i : i + BATCH_SIZE] = image_features.cpu().numpy()
 
 embeddings.flush()
