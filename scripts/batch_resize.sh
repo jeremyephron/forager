84f9f555005298cc28285180b408d2bcac7bbd37
Mihir Garimella <mihir.garimella@gmail.com>
2021-04-27 20:54:35 -0700

    cleanup

--- scripts/batch_resize.sh
+++ scripts/batch_resize.sh
@@ -3,6 +3,5 @@ NEW_SIZE=300x200
 DST_DIR=gs://foragerml/thumbnails/2d2b13f9-3b30-4e51-8ab9-4e8a03ba1f03/
 
 cp -r $SRC_DIR thumbs
-(cd thumbs && rename.ul .jpeg .jpg *.jpeg)
-(cd thumbs && mogrify -resize $NEW_SIZE -monitor *.jpg)
+(cd thumbs && rename.ul .jpeg .jpg *.jpeg && mogrify -resize $NEW_SIZE -monitor *.jpg)
 gsutil -m cp -r thumbs/* $DST_DIR
