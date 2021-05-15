97bf9bd189da43d8b6fbec128d90ec12b88ddc90
Mihir Garimella <mihir.garimella@gmail.com>
2021-05-15 14:32:52 -0700

    redirect to waymo

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,16 +6,20 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
+  Redirect,
 } from "react-router-dom";
 
 import TimeAgo from "javascript-time-ago";
 import en from "javascript-time-ago/locale/en";
 TimeAgo.addDefaultLocale(en);
 
+const DEFAULT_DATASET_NAME = "waymo";
+
 ReactDOM.render(
   <React.StrictMode>
     <Router>
       <Switch>
+        <Redirect exact from="/" to={`/${DEFAULT_DATASET_NAME}`} />
         <Route path="/:datasetName" children={<App />} />
       </Switch>
     </Router>
