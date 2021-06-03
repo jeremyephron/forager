6b64d516ebb9f8b6d5a36142cc817505ae67b677
Mihir Garimella <mihir.garimella@gmail.com>
2021-06-03 16:47:02 -0700

    migration for updated models
    no indexes yet

--- forager_server/forager_server_api/models.py
+++ forager_server/forager_server_api/models.py
@@ -1,44 +1,57 @@
 from django.db import models
 
 
+MEDIUM_STRING_LENGTH = 300
+LONG_STRING_LENGTH = 600
+
+
 class Dataset(models.Model):
     name = models.SlugField(unique=True)
-    directory = models.CharField(max_length=600)
-    val_directory = models.CharField(max_length=600, blank=True)
-    index_id = models.CharField(max_length=300, blank=True)
+    train_directory = models.CharField(max_length=LONG_STRING_LENGTH)
+    val_directory = models.CharField(max_length=LONG_STRING_LENGTH, blank=True)
+    index_id = models.CharField(max_length=MEDIUM_STRING_LENGTH)
     hidden = models.BooleanField(default=False)
+
+
+class User(models.Model):
+    email = models.EmailField(unique=True)
 
 
 class DatasetItem(models.Model):
     dataset = models.ForeignKey(Dataset, on_delete=models.CASCADE)
-    identifier = models.CharField(max_length=300)
-    path = models.CharField(max_length=600)
-    google = models.BooleanField(default=False)
+    identifier = models.CharField(max_length=MEDIUM_STRING_LENGTH)
+    path = models.CharField(max_length=LONG_STRING_LENGTH)
     is_val = models.BooleanField(default=False)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=MEDIUM_STRING_LENGTH, unique=True)
+
+
+class Mode(models.Model):
+    name = models.CharField(max_length=MEDIUM_STRING_LENGTH, unique=True)
 
 
 class Annotation(models.Model):
     dataset_item = models.ForeignKey(DatasetItem, on_delete=models.CASCADE)
-    label_function = models.CharField(max_length=300)
-    label_category = models.CharField(max_length=300)
-    label_type = models.CharField(max_length=300)
-    label_data = models.CharField(max_length=5000)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    mode = models.ForeignKey(Mode, on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
-    last_updated = models.DateTimeField(auto_now=True)
-
-
-class EmbeddingSet(models.Model):
-    dataset = models.ForeignKey(Dataset, on_delete=models.CASCADE)
-    index_name = models.CharField(max_length=1000)
+    bbox_x1 = models.FloatField(null=True)
+    bbox_y1 = models.FloatField(null=True)
+    bbox_x2 = models.FloatField(null=True)
+    bbox_y2 = models.FloatField(null=True)
+    misc_data = models.JSONField(default=dict)
 
 
 class DNNModel(models.Model):
     dataset = models.ForeignKey(Dataset, on_delete=models.CASCADE)
     name = models.SlugField()
-    model_id = models.CharField(max_length=300, unique=True)
-    checkpoint_path = models.TextField(null=True)
-    output_directory = models.TextField(null=True)
+    model_id = models.CharField(max_length=MEDIUM_STRING_LENGTH, unique=True)
+    checkpoint_path = models.CharField(max_length=LONG_STRING_LENGTH, null=True)
+    output_directory = models.CharField(max_length=LONG_STRING_LENGTH, null=True)
     last_updated = models.DateTimeField(auto_now=True)
     category_spec = models.JSONField(default=dict)
-    resume_model_id = models.CharField(max_length=300, null=True)
+    resume_model_id = models.CharField(max_length=MEDIUM_STRING_LENGTH, null=True)
     epoch = models.IntegerField(default=0)
