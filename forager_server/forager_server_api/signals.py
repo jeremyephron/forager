4d49796a93487eb99065d297715f4380f888922e
Mihir Garimella <mihir.garimella@gmail.com>
2021-06-27 20:53:17 -0400

    garbage collect old category counts

--- forager_server/forager_server_api/signals.py
+++ forager_server/forager_server_api/signals.py
@@ -1,13 +1,14 @@
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
-from .models import Category, Mode, Annotation, CategoryCount
+from .models import Annotation, CategoryCount
+
 
 @receiver(post_save, sender=Annotation)
 def increment_category_count(sender, instance, **kwargs):
     category_count, _ = CategoryCount.objects.get_or_create(
         dataset=instance.dataset_item.dataset,
         category=instance.category,
-        mode=instance.mode
+        mode=instance.mode,
     )
     category_count.count += 1
     category_count.save()
@@ -15,25 +16,13 @@ def increment_category_count(sender, instance, **kwargs):
 
 @receiver(post_delete, sender=Annotation)
 def decrement_category_count(sender, instance, **kwargs):
-    category_count, _ = CategoryCount.objects.get_or_create(
+    category_count, _ = CategoryCount.objects.get(
         dataset=instance.dataset_item.dataset,
         category=instance.category,
-        mode=instance.mode
+        mode=instance.mode,
     )
-    category_count.count -= 1
-    category_count.save()
-
-# for dataset in Dataset.objects.filter():
-#     x = Annotation.objects.filter(dataset_item__in=dataset.datasetitem_set.filter()).values("category__name", "mode__name").annotate(n=Count("pk"))
-#     for c in x:
-#         cat = Category.objects.get(name=c["category__name"])
-#         mode = Mode.objects.get(name=c["mode__name"])
-#         print(dataset.name, cat.name, mode.name, c["n"])
-#         category_count, _ = CategoryCount.objects.get_or_create(
-#             dataset=dataset,
-#             category=cat,
-#             mode=mode
-#         )
-#         category_count.count = c["n"]
-#         category_count.save()
-#     break
+    if category_count.count == 0:
+        category_count.delete()
+    else:
+        category_count.count -= 1
+        category_count.save()
---
+++ launcher/main.py
@@ -0,0 +1,6 @@
+def add_dataset():
+    pass
+
+
+def delete_dataset():
+    pass
